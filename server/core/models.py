afbf672b9260dba287b5d2843912758279afcdc5
nhrnhr0 <nhrnhr0@gmail.com>
2022-04-08 12:14:14 +0300

    adding markers colors

--- server/core/models.py
+++ server/core/models.py
@@ -1,13 +1,16 @@
+from ast import Return
 from django.db import models
 
 # Create your models here.
 class MapArea(models.Model):
     name = models.CharField(max_length=254, unique=True)
-    
+    markerUrl = models.CharField(max_length=254, null=True, blank=True)
+    def __str__(self):
+        return self.name
     pass
 class MapPoint(models.Model):
     name = models.CharField(max_length=254, unique=True)
-    lat = models.DecimalField('Latitude', max_digits=10, decimal_places=8)
+    lat = models.DecimalField('Latitude', max_digits=11, decimal_places=8)
     lng = models.DecimalField('Longitude', max_digits=11, decimal_places=8)
-    area = models.ForeignKey(to=MapArea, null=True, on_delete=models.SET_NULL)
+    area = models.ForeignKey(to=MapArea, null=True, blank=True, on_delete=models.SET_NULL)
     
--- server/core/serializers.py
+++ server/core/serializers.py
@@ -3,6 +3,7 @@
 from rest_framework import serializers
 from .models import MapPoint
 class MapPointSerializer(serializers.ModelSerializer):
+    marker = serializers.CharField(source='area.markerUrl', read_only=True)
     class Meta:
         model = MapPoint
-        fields = ('name','lat','lng',)
+        fields = ('name','lat','lng', 'marker')
